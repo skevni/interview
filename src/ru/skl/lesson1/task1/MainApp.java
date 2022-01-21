19643ed94aacab8ea15d7f950de70e05b5d856b4
Evgeniy Sklyarov <skevni@gmail.com>
2022-01-21 19:30:53 +0300

    hw #1
    1. after code review

--- src/ru/skl/lesson1/task1/MainApp.java
+++ src/ru/skl/lesson1/task1/MainApp.java
@@ -2,8 +2,8 @@ package ru.skl.lesson1.task1;
 
 public class MainApp {
     public static void main(String[] args) {
-        PersonBuilder personBuilder = new PersonBuilder(new Person());
+        Person person = new Person.Builder().firstName("Iam").age(1).address("moscow").country("ru").build();
+        System.out.println(person);
 
-        System.out.println(personBuilder.age(1).address("moscow").country("ru").build());
     }
 }
--- src/ru/skl/lesson1/task1/Person.java
+++ src/ru/skl/lesson1/task1/Person.java
@@ -1,80 +1,26 @@
 package ru.skl.lesson1.task1;
 
 public class Person {
-    private String firstName;
-    private String lastName;
-    private String middleName;
-    private String country;
-    private String address;
-    private String phone;
-    private int age;
-    private String gender;
+    private final String firstName;
+    private final String lastName;
+    private final String middleName;
+    private final String country;
+    private final String address;
+    private final String phone;
+    private final int age;
+    private final String gender;
 
-    public Person() {
-    }
+    private Person(Builder builder) {
 
-    public String getFirstName() {
-        return firstName;
-    }
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.middleName = builder.middleName;
+        this.country = builder.country;
+        this.address = builder.address;
+        this.age = builder.age;
+        this.phone = builder.phone;
+        this.gender = builder.gender;
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 
     @Override
@@ -89,5 +35,65 @@ public class Person {
                 ", age=" + age +
                 ", gender='" + gender + '\'' +
                 '}';
+    }
+
+    public static final class Builder {
+        private String firstName;
+        private String lastName;
+        private String middleName;
+        private String country;
+        private String address;
+        private String phone;
+        private int age;
+        private String gender;
+
+        public Builder() {
+
+        }
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder middleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder country(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder gender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+
+        public Person build() {
+            return new Person(this);
+        }
     }
 }
